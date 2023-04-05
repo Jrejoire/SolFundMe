0dd6210728b9a3776e9bcc1b4853a919288d3b52
Gregoire <gregoire.cottin@skezi.eu>
2023-04-06 01:57:59 +0200

    first commit

---
+++ programs/solfundme/src/lib.rs
@@ -0,0 +1,244 @@
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+
+declare_id!("9kYU5QF9ecxxn7PxijJ1BCGcsos9fPJmvTF34yANWEPq");
+
+#[program]
+pub mod solfundme {
+    use super::*;
+
+    pub fn init_campaign(
+        ctx: Context<InitCampaign>,
+        name: String,
+        description: String,
+        goal: u64,
+        duration_seconds: i64,
+    ) -> ProgramResult {
+        let campaign = &mut ctx.accounts.campaign;
+        campaign.goal = goal;
+        campaign.owner = *ctx.accounts.user.key;
+        campaign.pledged = 0;
+        campaign.pledgers = Vec::new();
+        campaign.name = name;
+        campaign.description = description;
+        campaign.is_active = true;
+        campaign.is_funded = false;
+
+        let clock = Clock::get()?;
+        campaign.timestamp_start = clock.unix_timestamp;
+        campaign.timestamp_end = clock.unix_timestamp + duration_seconds;
+
+        Ok({})
+    }
+
+    pub fn send_pledge(ctx: Context<SendPledge>, amount: u64) -> ProgramResult {
+        let campaign = &mut ctx.accounts.campaign;
+        let user = &mut ctx.accounts.user;
+
+        let is_pledger = campaign
+            .pledgers
+            .iter()
+            .any(|pledger| (pledger.address == *user.key) & (pledger.pledge > 0));
+
+        if is_pledger {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        let txn = anchor_lang::solana_program::system_instruction::transfer(
+            &user.key(),
+            &campaign.key(),
+            amount,
+        );
+
+        anchor_lang::solana_program::program::invoke(
+            &txn,
+            &[user.to_account_info(), campaign.to_account_info()],
+        )?;
+
+        let pledger = Pledger {
+            address: *user.key,
+            pledge: amount,
+        };
+
+        campaign.pledgers.push(pledger);
+        campaign.pledged += amount;
+
+        campaign.is_funded = campaign.pledged >= campaign.goal;
+
+        Ok({})
+    }
+
+    pub fn cancel_pledge(ctx: Context<CancelPledge>) -> ProgramResult {
+        let campaign = &mut ctx.accounts.campaign;
+        let user = &mut ctx.accounts.user;
+
+        let is_pledger = campaign
+            .pledgers
+            .iter()
+            .any(|pledger| (pledger.address == *user.key) & (pledger.pledge > 0));
+
+        if !is_pledger {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        let clock = Clock::get()?;
+        let current_timestamp = clock.unix_timestamp;
+        let is_campaign_over = current_timestamp > campaign.timestamp_end;
+        if is_campaign_over {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        let index = campaign
+            .pledgers
+            .iter()
+            .position(|pledger| pledger.address == *user.key)
+            .unwrap();
+
+        let pledger = &campaign.pledgers[index];
+
+        if pledger.address != user.key() {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        **campaign.to_account_info().try_borrow_mut_lamports()? -= pledger.pledge;
+        **user.to_account_info().try_borrow_mut_lamports()? += pledger.pledge;
+        campaign.pledged -= pledger.pledge;
+
+        let _ = &campaign.pledgers.remove(index);
+
+        Ok({})
+    }
+
+    pub fn check_campaign(ctx: Context<CheckCampaign>) -> ProgramResult {
+        let campaign = &mut ctx.accounts.campaign;
+
+        let clock = Clock::get()?;
+        let current_timestamp = clock.unix_timestamp;
+        let is_campaign_over = current_timestamp > campaign.timestamp_end;
+
+        if !is_campaign_over {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        if !campaign.is_active {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        campaign.is_funded = campaign.pledged >= campaign.goal;
+        campaign.is_active = false;
+
+        Ok({})
+    }
+
+    pub fn claim_funds(ctx: Context<ClaimFunds>) -> ProgramResult {
+        let campaign = &mut ctx.accounts.campaign;
+        let user = &mut ctx.accounts.user;
+
+        if campaign.is_active {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        if campaign.is_funded {
+            if *user.key != campaign.owner {
+                return Err(ProgramError::InvalidAccountData);
+            }
+
+            **campaign.to_account_info().try_borrow_mut_lamports()? -= campaign.pledged;
+            **user.to_account_info().try_borrow_mut_lamports()? += campaign.pledged;
+
+            campaign.pledged = 0;
+        } else {
+            let is_pledger = campaign
+                .pledgers
+                .iter()
+                .any(|pledger| (pledger.address == *user.key) & (pledger.pledge > 0));
+
+            if !is_pledger {
+                return Err(ProgramError::InvalidAccountData);
+            }
+
+            let index = campaign
+                .pledgers
+                .iter()
+                .position(|pledger| pledger.address == *user.key)
+                .unwrap();
+
+            let pledge = campaign.pledgers[index].pledge;
+            if pledge == 0 {
+                return Err(ProgramError::InvalidAccountData);
+            }
+
+            **campaign.to_account_info().try_borrow_mut_lamports()? -= pledge;
+            **user.to_account_info().try_borrow_mut_lamports()? += pledge;
+
+            campaign.pledgers[index].pledge = 0;
+        }
+        
+        Ok({})
+    }
+}
+
+#[derive(Accounts)]
+pub struct InitCampaign<'info> {
+    #[account(init, payer=user, space=5000, seeds=[b"solfundme", user.key().as_ref()], bump)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct SendPledge<'info> {
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CancelPledge<'info> {
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CheckCampaign<'info> {
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct ClaimFunds<'info> {
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Campaign {
+    goal: u64,
+    pledged: u64,
+    pledgers: Vec<Pledger>,
+    name: String,
+    description: String,
+    owner: Pubkey,
+    timestamp_start: i64,
+    timestamp_end: i64,
+    is_active: bool,
+    is_funded: bool,
+}
+
+#[derive(Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct Pledger {
+    address: Pubkey,
+    pledge: u64,
+}
---
+++ tests/solfundme.ts
@@ -0,0 +1,158 @@
+import * as anchor from "@coral-xyz/anchor";
+import { expect } from 'chai';
+import { Solfundme } from "../target/types/solfundme";
+const { SystemProgram } = anchor.web3
+import { Connection, PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { utils } from "@project-serum/anchor";
+
+describe("solfundme", () => {
+  const program = anchor.workspace.Solfundme as anchor.Program<Solfundme>;
+  const contributer = anchor.web3.Keypair.generate();
+  const campainOwner = anchor.web3.Keypair.generate();
+  const campainOwner_2 = anchor.web3.Keypair.generate();
+  const connection = new Connection("http://127.0.0.1:8899");
+
+  it("should fund wallets", async () => {
+    const txOwner = await connection.requestAirdrop(campainOwner.publicKey, 1 * LAMPORTS_PER_SOL);
+    const txOwner_2 = await connection.requestAirdrop(campainOwner_2.publicKey, 1 * LAMPORTS_PER_SOL);
+    const txUser = await connection.requestAirdrop(contributer.publicKey, 2 * LAMPORTS_PER_SOL);
+    await new Promise(resolve => setTimeout(resolve, 2 * 1000));
+    expect(txOwner).to.not.equal(null);
+    expect(txOwner_2).to.not.equal(null);
+    expect(txUser).to.not.equal(null);
+  })
+
+  const [campaign] = PublicKey.findProgramAddressSync([
+    utils.bytes.utf8.encode("solfundme"),
+    campainOwner.publicKey.toBuffer(),
+  ], program.programId);
+
+  it("should init campaign", async () => {
+    await program.methods.initCampaign("campaign_1", "This is to fund my degen lifestyle.", new anchor.BN(0.5 * LAMPORTS_PER_SOL), new anchor.BN(3))
+      .accounts({
+        campaign,
+        user: campainOwner.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([campainOwner])
+      .rpc();
+
+    const account = await program.account.campaign.fetch(campaign);
+    expect(account.pledged.toString()).to.equal("0");
+    expect(account.goal.toString()).to.equal((0.5 * LAMPORTS_PER_SOL).toString());
+  })
+
+  it("should add pledge to the campaign", async () => {
+    await program.methods.sendPledge(new anchor.BN(0.5 * LAMPORTS_PER_SOL)).accounts({ campaign, user: contributer.publicKey, systemProgram: SystemProgram.programId }).signers([contributer]).rpc();
+
+    const account = await program.account.campaign.fetch(campaign);
+    expect(account.pledgers[0].address.toString()).to.equal(contributer.publicKey.toString());
+    expect(account.pledgers[0].pledge.toString()).to.equal(`${0.5 * LAMPORTS_PER_SOL}`);
+    expect(account.pledged.toString()).to.equal(`${0.5 * LAMPORTS_PER_SOL}`);
+  })
+
+  it("should return an error if pledger already pledged", async () => {
+    let tx = undefined;
+    try {
+      tx = await program.methods.sendPledge(new anchor.BN(0.4 * LAMPORTS_PER_SOL)).accounts({ campaign, user: contributer.publicKey, systemProgram: SystemProgram.programId }).signers([contributer]).rpc();
+    } catch (err) {
+      expect(err).to.not.equal(null);
+    }
+    expect(tx).to.equal(undefined);
+  })
+
+  it("should cancel pledge", async () => {
+    await program.methods.cancelPledge().accounts({ campaign, user: contributer.publicKey, systemProgram: SystemProgram.programId }).signers([contributer]).rpc();
+
+    const account = await program.account.campaign.fetch(campaign);
+    expect(account.pledgers.length).to.equal(0);
+    expect(account.pledgers.some(pledger => pledger.address.toString() === contributer.publicKey.toString())).to.equal(false);
+  })
+
+  it("should return an error if pledger doesn't exist", async () => {
+    let tx = undefined;
+    try {
+      tx = await program.methods.cancelPledge().accounts({ campaign, user: contributer.publicKey, systemProgram: SystemProgram.programId }).signers([contributer]).rpc();
+    } catch (err) {
+      expect(err).to.not.equal(null);
+    }
+    expect(tx).to.equal(undefined);
+  })
+
+  it("should check campaign and find out that it's over and funded", async () => {
+    await program.methods.sendPledge(new anchor.BN(0.5 * LAMPORTS_PER_SOL)).accounts({ campaign, user: contributer.publicKey, systemProgram: SystemProgram.programId }).signers([contributer]).rpc();
+    await new Promise(resolve => setTimeout(resolve, 4 * 1000));
+    await program.methods.checkCampaign().accounts({ campaign, user: campainOwner.publicKey, systemProgram: SystemProgram.programId }).signers([campainOwner]).rpc();
+
+    const account = await program.account.campaign.fetch(campaign);
+    expect(account.isFunded).to.equal(true);
+    expect(account.isActive).to.equal(false);
+  })
+
+  it("should not refund user on claim if campaign is over and funded", async () => {
+    let tx = undefined;
+    try {
+      tx = await program.methods.claimFunds().accounts({ campaign, user: contributer.publicKey, systemProgram: SystemProgram.programId }).signers([contributer]).rpc();
+    } catch (err) {
+      expect(err).to.not.equal(null);
+    }
+    expect(tx).to.equal(undefined);
+  })
+
+  it("should fund owner on claim if campaign is over and funded", async () => {
+    await program.methods.claimFunds().accounts({ campaign, user: campainOwner.publicKey, systemProgram: SystemProgram.programId }).signers([campainOwner]).rpc();
+    const account = await program.account.campaign.fetch(campaign);
+
+    expect(account.pledged.toString()).to.be.equal("0");
+  })
+
+  it("should return an error when contributer attempts to claim funds on over and funded campaign", async () => {
+    let tx = undefined;
+    try {
+      await program.methods.claimFunds().accounts({ campaign, user: contributer.publicKey, systemProgram: SystemProgram.programId }).signers([contributer]).rpc();
+    } catch (err) {
+      expect(err).to.not.equal(null);
+    }
+    expect(tx).to.equal(undefined);
+  })
+
+  it("should return an error when user attempts to cancel pledge on over and funded campaign", async () => {
+    let tx = undefined;
+    try {
+      await program.methods.cancelPledge().accounts({ campaign, user: contributer.publicKey, systemProgram: SystemProgram.programId }).signers([contributer]).rpc();
+    } catch (err) {
+      expect(err).to.not.equal(null);
+    }
+    expect(tx).to.equal(undefined);
+  })
+
+  const [campaign_2] = PublicKey.findProgramAddressSync([
+    utils.bytes.utf8.encode("solfundme"),
+    campainOwner_2.publicKey.toBuffer(),
+  ], program.programId);
+
+  it("should check that campaign is over but not funded", async () => {
+    await program.methods.initCampaign("campaign_2", "This is to make a cool crypto project.", new anchor.BN(0.5 * LAMPORTS_PER_SOL), new anchor.BN(0))
+      .accounts({
+        campaign: campaign_2,
+        user: campainOwner_2.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([campainOwner_2])
+      .rpc();
+
+    await program.methods.sendPledge(new anchor.BN(0.1 * LAMPORTS_PER_SOL)).accounts({ campaign: campaign_2, user: contributer.publicKey, systemProgram: SystemProgram.programId }).signers([contributer]).rpc();
+    await program.methods.checkCampaign().accounts({ campaign: campaign_2, user: campainOwner_2.publicKey, systemProgram: SystemProgram.programId }).signers([campainOwner_2]).rpc();
+
+    const account = await program.account.campaign.fetch(campaign_2);
+    expect(account.pledgers.length).to.equal(1);
+    expect(account.isFunded).to.equal(false);
+    expect(account.isActive).to.equal(false);
+  })
+
+  it("should allow contributers to claim back funds if campaign is over but not funded", async () => {
+    await program.methods.claimFunds().accounts({ campaign: campaign_2, user: contributer.publicKey, systemProgram: SystemProgram.programId }).signers([contributer]).rpc();
+    const account = await program.account.campaign.fetch(campaign_2);
+    expect(account.pledgers[0].pledge.toString()).to.equal("0");
+  })
+});
